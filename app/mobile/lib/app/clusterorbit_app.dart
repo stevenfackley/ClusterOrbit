b98af585d8b5d8d32b408710272a88c4706bc1f9
Steve Ackley <stevenfackley@gmail.com>
2026-04-16 16:33:30 -0400

    feat: wire SqfliteSnapshotStore into OrbitShell with cache-first bootstrap

--- app/mobile/lib/app/clusterorbit_app.dart
+++ app/mobile/lib/app/clusterorbit_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../core/connectivity/cluster_connection.dart';
+import '../core/sync_cache/snapshot_store.dart';
 import '../core/theme/clusterorbit_theme.dart';
 import '../shared/widgets/orbit_shell.dart';
 
@@ -8,9 +9,11 @@ class ClusterOrbitApp extends StatelessWidget {
   const ClusterOrbitApp({
     super.key,
     this.connection,
+    this.store,
   });
 
   final ClusterConnection? connection;
+  final SnapshotStore? store;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +23,7 @@ class ClusterOrbitApp extends StatelessWidget {
       theme: ClusterOrbitTheme.light(),
       darkTheme: ClusterOrbitTheme.dark(),
       themeMode: ThemeMode.dark,
-      home: OrbitShell(connection: connection),
+      home: OrbitShell(connection: connection, store: store),
     );
   }
 }
